acb13ca03dd75fb7db3e557d55f4209f1fb06597
Ysungho <ysh01022320@naver.com>
2024-04-03 16:01:26 +0900

    spring->spring boot, spring data jpa

---
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,35 @@
+package org.zerock.b01.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false) //컬럼의 길이와 null허용여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    public void change(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
